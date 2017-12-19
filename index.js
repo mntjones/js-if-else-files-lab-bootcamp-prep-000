78037902477ce90270da580c7068eb37f9406517
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-19 20:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var name = "Bob"
-var height = "74"
+var name = "Susan"
+var height = "70"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
 
-if (name === "Susan") {
-  console.log("The name is correct")
-}
-else {
-  console.log("Expected the name to be Susan")
-}
+//if (name === "Susan") {
+//  console.log("The name is correct")
+//}
+//else {
+//  console.log("Expected the name to be Susan")
+//}
